a0f71e1337615f97ad1f1d527584ddca9cac0c98
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2021-03-20 22:32:23 +0900

    Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
 import "./app.css";
 import React, { useState } from "react";
+import { Link, Route, Router, Switch } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 export default function App() {
+  return (
+    <Router history={history}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">Abount</Link>
+        </li>
+        <li>
+          <Link to="/counter">Counter</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/counter">
+          <Counter />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Counter() {
   const [count, setCount] = useState(0);
 
   const handleClick = () => {
@@ -10,6 +55,7 @@ export default function App() {
 
   return (
     <div>
+      <h2>Counter</h2>
       <div>
         <span>Count:</span>
         <span>{count}</span>
